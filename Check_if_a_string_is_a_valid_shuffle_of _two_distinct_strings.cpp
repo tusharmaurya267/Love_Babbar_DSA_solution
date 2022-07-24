3e419986514301d18193bb9eb269357157f60dec
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2022-07-24 11:17:28 +0530

    more

---
+++ Check_if_a_string_is_a_valid_shuffle_of _two_distinct_strings.cpp
@@ -0,0 +1,11 @@
+#include<bits.stdc++.h>
+using namespace std;
+int main(){
+    string str1,str2;
+    cin >> str1 >> str2;
+    for(int i=0;i<str.length();i++){
+        for( int i=0;i<str.length();i++){
+            
+        }
+    }
+}
