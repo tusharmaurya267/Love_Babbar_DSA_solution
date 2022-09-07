4e7b99831e0c18595739fda22f27550f0dbf3201
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2022-09-08 01:16:36 +0530

    ++

---
+++ Array/maximum_product_subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+      int mini=nums[0];
+      int maxi=nums[0];
+      int ans=nums[0];
+      for(int i=1;i<nums.size();i++){
+        if(nums[i]<0) swap(mini,maxi);
+        maxi=max(nums[i],nums[i]*maxi);
+        mini=min(nums[i],nums[i]*mini);
+        ans=max(maxi,ans);
+      }
+      return ans;
+
+    }
+};
